88accff4c3fb4ae67499f002905fbfd992906aeb
Joseph <sourpina@gmail.com>
2026-01-16 19:57:51 -0500

    Created complete ArrayList header file

---
+++ C++/ArrayList.hpp
@@ -0,0 +1,32 @@
+#ifndef ARRAYLIST
+#define ARRAYLIST
+
+template <typename T>
+class ArrayList {
+    private:
+        T* list;
+        int count;
+        int capacity;
+
+        void copy(const ArrayList<T>& other);
+        void destroy();
+    public:
+        ArrayList(const int& size = 10);
+        ArrayList(const ArrayList<T>& other);
+        ~ArrayList();
+
+        const ArrayList<T>& operator=(const ArrayList<T>& other);
+
+        int getCapacity();
+        int getCount();
+        int indexOf(T item);
+        bool isEmpty();
+        bool isFull();
+
+        void insert(T item);
+        void insertAt(T item, int index);
+        void remove(T item);
+        void removeAt(int index);
+};
+
+#endif
