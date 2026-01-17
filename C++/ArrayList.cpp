b09ea29ba13a2e1bf69a585266584610fe06ea39
Joseph <sourpina@gmail.com>
2026-01-16 20:09:09 -0500

    Implemented constructors and helper functions

---
+++ C++/ArrayList.cpp
@@ -0,0 +1,48 @@
+#include <ArrayList.hpp>
+
+// Constructor
+template <typename T>
+ArrayList<T>::ArrayList(const int& size) {
+    count = 0;
+    capacity = size;
+    list = new T[capacity];
+}
+
+// Copy Constructor
+template <typename T>
+ArrayList<T>::ArrayList(const ArrayList<T>& other) {
+    copy(other);
+}
+
+// Destructor
+template <typename T>
+ArrayList<T>::~ArrayList() {
+    destroy();
+}
+
+// Overloaded Assignment Operator
+template <typename T>
+const ArrayList<T>& ArrayList<T>::operator=(const ArrayList<T>& other) {
+    if (this != &other)
+        copy(other);
+    
+    return *this;
+}
+
+// Copy Helper Function
+template <typename T>
+void ArrayList<T>::copy(const ArrayList<T>& other) {
+    count = other.count;
+    capacity = other.capacity;
+    list = new T[capacity];
+    for (int i = 0; i < count; i++)
+        list[i] = other.list[i];
+}
+
+// Destory Helper Function
+template <typename T>
+void ArrayList<T>::destroy() {
+    delete[] list;
+}
+
+
