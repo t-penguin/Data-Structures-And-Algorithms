02da4a6ebedc70c2eca8eded6d73e030c6ff8e29
Joseph <sourpina@gmail.com>
2026-01-17 23:35:43 -0500

    Implemented copy and destroy helper functions

--- C++/LinkedList.cpp
+++ C++/LinkedList.cpp
@@ -19,3 +19,38 @@ template <typename T>
 LinkedList<T>::~LinkedList() {
     destroy();
 }
+
+// Copy helper function
+template <typename T>
+void LinkedList<T>::copy(const LinkedList<T>& other) {
+    if (this == &other)
+        return;
+    
+    size = other.size;
+    if (other.first == nullptr)
+        return;
+    
+    // Copy first node
+    Node<T>* otherNode = other.first;
+    first = new Node<T>(otherNode->info);
+
+    // Iterate through list, copying nodes
+    Node<T>* curNode = first;
+    otherNode = otherNode->next;
+    while (otherNode != nullptr)
+        curNode->next = new Node<T>(otherNode->info);
+    
+    curNode->next = nullptr;
+}
+
+// Destroy helper function
+template <typename T>
+void LinkedList<T>::destroy() {
+    size = 0;
+    Node<T>* curNode;
+    while (first != nullptr) {
+        curNode = first;
+        first = first->next;
+        delete curNode;
+    }
+}
