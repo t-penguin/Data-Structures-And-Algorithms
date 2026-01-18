08dbd3765f098e71c9d479a018da6320e12c574b
Joseph <sourpina@gmail.com>
2026-01-17 23:31:10 -0500

    Added missing function descriptions

--- C++/LinkedList.cpp
+++ C++/LinkedList.cpp
@@ -1,17 +1,20 @@
 #include <LinkedList.hpp>
 #include <stdexcept>
 
+// Constructor
 template <typename T>
 LinkedList<T>::LinkedList() {
     size = 0;
     first = nullptr;
 }
 
+// Copy Constructor
 template <typename T>
 LinkedList<T>::LinkedList(const LinkedList<T>& other) {
     copy(other);
 }
 
+// Destructor
 template <typename T>
 LinkedList<T>::~LinkedList() {
     destroy();
