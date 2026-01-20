ac571e338ee8fdc5737861b279cf98fe306756aa
Joseph <sourpina@gmail.com>
2026-01-20 00:08:35 -0500

    Added Linked Stack header file

---
+++ C++/LinkedStack.hpp
@@ -0,0 +1,29 @@
+#ifndef LINKEDSTACK
+#define LINKEDSTACK
+
+#include "LinkedList.hpp"
+
+template <typename T>
+class LinkedStack : protected LinkedList {
+    private:
+        
+    public:
+        LinkedStack();
+        LinkedStack(const LinkedStack<T>& other);
+        ~LinkedStack();
+
+        const LinkedStack<T>& operator=(const LinkedStack<T>& other);
+
+        int getSize() const;
+        bool isEmpty() const;
+        int indexOf(T item) const;
+        bool contains(T item) const;
+        const T& top() const;
+        T& top();
+        
+        void push(T item);
+        void pop(T item);
+        void clear();
+};
+
+#endif
